15e10b05a7bd22ceea6e1f69f2be01869ef3dfc9
molbrown <molly@spreedly.com>
2021-12-01 16:11:07 -0500

    Clean up part1 with count/2 instead of map |> sum

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,30 +1,14 @@
 defmodule AdventOfCode.Day01 do
   def part1(input) do
-    count =
     Stream.chunk_every(input, 2, 1, :discard)
     |> Enum.to_list()
-    |> Enum.map( fn [a,b] ->
-        case a do
-          a when a < b -> 1
-          _ -> 0
-        end
-      end)
-    |> Enum.sum()
-
-    IO.puts "\nthe number of times a depth measurement increases is: \ #{count} \."
-
-    count
+    |> Enum.count( fn [a,b] -> a < b end)
   end
 
   def part2(input) do
-    count =
     Stream.chunk_every(input, 3, 1, :discard)
     |> Enum.to_list()
     |> Enum.map( fn chunk -> Enum.sum(chunk) end)
     |> part1()
-
-    IO.puts "\nthe number of times the sum of measurements in this sliding window increases is: \ #{count} \."
-
-    count
   end
 end
