c299caf78373b56ffcf3e344b517b08df6e6b50e
molbrown <molly@spreedly.com>
2021-12-04 14:37:52 -0500

    Day 4 Part 2 and a bit of refactor

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,32 +1,41 @@
 defmodule AdventOfCode.Day04 do
+
   def part1(input) do
-    draws =
+    {win, win_num} = play_game(boards(input), draws(input), "first_win")
+
+    calculate_score(win, win_num)
+  end
+
+  def part2(input) do
+    {last_win, last_num} = play_game(boards(input), draws(input), "last_win")
+
+    calculate_score(last_win, last_num)
+  end
+
+  defp draws(input) do
     Enum.at(input, 0)
     |> String.split(",")
+  end
 
-    # Create 2D Lists representing each bingo board
-    boards =
+  # Create 2D Lists representing each bingo board
+  defp boards(input) do
     Enum.drop(input, 1)
     |> Enum.map(fn x -> String.split(x, " ", trim: true) end)
     |> Enum.chunk_every(5)
-
-    {win, win_num} = 
-    draws
-    |> Enum.reduce_while(boards, fn draw, b -> 
-      last_draw = Enum.find_index(draws, fn x -> x == draw end)
-
-      if bingo(b), do: {:halt, {b, Enum.at(draws, last_draw - 1)}}, else: {:cont, mark_boards(draw, b)}
-    end)
-    
-    win
-    |> Enum.find(fn board -> horizontal(board) || vertical(board) end)
-    |> Enum.flat_map(fn row -> Enum.reject(row, fn x -> x == "x" end) end)
-    |> Enum.map(fn x -> String.to_integer(x) end)
-    |> Enum.sum()
-    |> Kernel.*(String.to_integer(win_num))
   end
 
-  def part2(_args) do
+  defp play_game(boards, draws, "first_win") do
+    draws
+    |> Enum.reduce_while(boards, fn draw, b -> 
+      if bingo(b), do: {:halt, {b, last_draw(draws, draw)}}, else: {:cont, mark_boards(draw, b)}
+    end)
+  end
+
+  defp play_game(boards, draws, "last_win") do
+    draws
+    |> Enum.reduce_while(boards, fn draw, b -> 
+      if bingo(b), do: {:halt, {b, last_draw(draws, draw)}}, else: {:cont, filter_bingo(draw, b)}
+    end)
   end
 
   defp mark_boards(draw, boards)do
@@ -46,6 +55,20 @@ defmodule AdventOfCode.Day04 do
     Enum.any?(boards, fn b -> horizontal(b) || vertical(b) end)
   end
 
+  defp filter_bingo(draw, boards) when length(boards) == 1 do
+    mark_boards(draw, boards)
+  end
+
+  defp filter_bingo(draw, boards) do
+    mark_boards(draw, boards)
+    |> Enum.reject(fn b -> horizontal(b) || vertical(b) end)
+  end
+
+  defp last_draw(draws, draw) do
+    Enum.find_index(draws, fn x -> x == draw end)
+    |> (&(Enum.at(draws, &1 - 1))).()
+  end
+
   defp horizontal(board) do
     board
     |> Enum.any?(fn row -> Enum.all?(row, fn x -> x == "x" end) end)
@@ -54,5 +77,14 @@ defmodule AdventOfCode.Day04 do
   defp vertical(board) do
     (0..4)
     |> Enum.any?(fn col -> Enum.all?(board, fn row -> Enum.at(row, col) == "x" end) end)
+  end
+
+  defp calculate_score(board, last_draw) do
+    board
+    |> Enum.find(fn board -> horizontal(board) || vertical(board) end)
+    |> Enum.flat_map(fn row -> Enum.reject(row, fn x -> x == "x" end) end)
+    |> Enum.map(fn x -> String.to_integer(x) end)
+    |> Enum.sum()
+    |> Kernel.*(String.to_integer(last_draw))
   end
 end
